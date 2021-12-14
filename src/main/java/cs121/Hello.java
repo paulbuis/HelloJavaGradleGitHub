9301227e24d41ea4790dd61ad99f6d26ee752478
Paul Buis <00pebuis@bsu.edu>
2021-12-14 08:37:07 -0500

    Initial Commit

---
+++ src/main/java/cs121/Hello.java
@@ -0,0 +1,9 @@
+package cs121;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Hello Gradle");
+        System.out.println("Hello GitHub");
+    }
+}
